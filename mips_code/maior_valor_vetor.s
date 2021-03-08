237155f93cf9a8fbed299708c944fd0e235d0f38
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-08 03:12:17 -0400

    Finalização do programa "maior valor do vetor"

--- mips_code/maior_valor_vetor.s
+++ mips_code/maior_valor_vetor.s
@@ -6,36 +6,70 @@
 
 ################# Data segment #####################
 .data
-msg1: .asciiz "Digite cada valor do vetor: "
-array1: .space 20 #aloca 20 espaços consecutivos de bytes, não inicializados.Para ser usado como um vetor de 5 inteiros
-
-
+meuVetor: .space 20 #alocate 20 bytes for 5 integers (5 numers * 4 bits)
+imprimeMaior: .asciiz "\n o maior numero eh: "
 ################# Code segment #####################
 .text
 .globl main
 main:           # main program entry
 
-    #printamos a mensagem
-    li $v0, 4 #o codigo 4 significa "carregar string"
-    la $a0, msg1  #colocar em a0 a mensagem que queremos imprimir
-    syscall #printamos a mensagem
+    #add values
+    addi $s0, $zero, 5
+    addi $s1, $zero, 2
+    addi $s2, $zero, 13
+    addi $s3, $zero, 3
+    addi $s4, $zero, 10
 
-    la $t0, array1 # carrega o endereço do array1 para $t0
+    #index == $t0
+    addi $t0, $zero,0
+    
+    sw $s0, meuVetor($t0)
+        addi $t0, $t0, 4
+    sw $s1, meuVetor($t0) #na segunda locacao
+        addi $t0, $t0, 4
+    sw $s2, meuVetor($t0) #na terceira locacao
+        addi $t0, $t0, 4
+    sw $s3, meuVetor($t0) #na quarta locacao
+        addi $t0, $t0, 4
+    sw $s4, meuVetor($t0) #na quinta locacao
+        addi $t0, $t0, 4
 
-    li $t1, 5 # t1 = 5
-    sw $t1, ($t0) # 1º elemento do array1 = 5
-    li $t1, 13 # t1 = 13
-    sw $t1, 4($t0) # 2º elemento do array1 = 13
-    li $t1, -7 # t1 = -7
-    sw $t1, 8($t0) # 3º elemento do array1 = -7
+    # clear t0
+    addi $t0, $zero, 0
 
-    #leitura do dado do usuário
-    li $v0, 5 # $v0 = integer read
-    syscall
-    move $t0, $v0 #registrador temporario t0 para mover o valor que o usuario colocou em v0 para o registrador t0
+    lw $s5, meuVetor, ($t0) # guarda o primeiro elemento do vetor como referencia de maior valor
+
+    while:
+        beq $t0,20,exit # vai para exit quando o index chegar na ultima posicao
+        lw $t6, meuVetor, ($t0) #carrega o primeiro elemento em t6
+
+        blt $t6, $s5, label #se: elemento atual do vetor(t6) < maior elemento do vetor até o momento(s5) desvia para o label
+            move $s5,$t6 #coloca o numero atual do array($t6) na variavel que identifica o maior valor do array ($s5))
+
+        # caso o valor atual lido no array não seja maior do que o valor guardado na variavel que identifica o maior numero:
+        label:
+        # acrescentar contador
+        addi $t0, $t0, 4
+
+        # printar o numero atual
+        #li $v0,1
+        #move $a0, $t6
+        #syscall 
+
+        j while
+
+    exit:
+
+        #imprimir maior numero
+        li $v0, 4
+        la $a0, imprimeMaior
+        syscall
+
+        li $v0,1
+        move $a0, $s5
+        syscall 
+
+        li $v0, 10      # Exit program
+        syscall
 
 
-
-
-li $v0, 10      # Exit program
-syscall

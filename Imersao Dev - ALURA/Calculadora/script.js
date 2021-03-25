099182561fde3a47341a53b43b18415bb2c74de9
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-24 23:22:49 -0300

    Exibir resultado com predefinições no CSS

--- Imersao Dev - ALURA/Calculadora/script.js
+++ Imersao Dev - ALURA/Calculadora/script.js
@@ -1,6 +1,8 @@
 var primeiroValor = parseInt(prompt("Digite o sprimeiro valor"))
 
 var segundoValor = parseInt(prompt("Digite o segundo valor:"))
-document.write(primeiroValor + " x "+ segundoValor +"\n")
 
-document.write(primeiroValor * segundoValor)
+document.getElementById("resultado").innerHTML=resultado;
+
+var resultado = (primeiroValor * segundoValor)
+document.getElementById("resultado").innerHTML=resultado;

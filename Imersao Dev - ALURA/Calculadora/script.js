a2ed0c4c4e69e1b5a54608f7b6a0762b52325618
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-24 23:03:02 -0300

    Create script.js

---
+++ Imersao Dev - ALURA/Calculadora/script.js
@@ -0,0 +1,6 @@
+var primeiroValor = parseInt(prompt("Digite o sprimeiro valor"))
+
+var segundoValor = parseInt(prompt("Digite o segundo valor:"))
+document.write(primeiroValor + " x "+ segundoValor +"\n")
+
+document.write(primeiroValor * segundoValor)

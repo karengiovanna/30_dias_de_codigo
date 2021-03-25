39d398494962d2d2c73e78ddba1c6c4ece074508
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-25 00:21:08 -0300

    Update script.js

--- Imersao Dev - ALURA/Calculadora/script.js
+++ Imersao Dev - ALURA/Calculadora/script.js
@@ -1,8 +1,5 @@
-var primeiroValor = parseInt(prompt("Digite o sprimeiro valor"))
+var primeiroValor = parseInt(prompt("Digite o primeiro valor"))
 
-var segundoValor = parseInt(prompt("Digite o segundo valor:"))
+var segundoValor = parseInt(prompt("Digite o segundo valor"))
 
-document.getElementById("resultado").innerHTML=resultado;
-
-var resultado = (primeiroValor * segundoValor)
-document.getElementById("resultado").innerHTML=resultado;
+document.write("<h2>" + primeiroValor * segundoValor + "</h2>");

8407af00ed55d0c81a7ac55bdaeae1c6ac02406f
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-25 00:43:58 -0300

    Update condicionais das operacoes

--- Imersao Dev - ALURA/Calculadora/script.js
+++ Imersao Dev - ALURA/Calculadora/script.js
@@ -1,11 +1,20 @@
+alert("Digite uma operação.")
+
+var operacao = parseInt(prompt("Multiplicação = 1; Soma = 2; Subtração = 3; Divisão = 4"))
+
 var primeiroValor = parseInt(prompt("Digite o primeiro valor"))
 
 var segundoValor = parseInt(prompt("Digite o segundo valor"))
 
-document.write("<h2>" + 
-               primeiroValor + 
-               " * " 
-               + segundoValor +
-               " = " +
-               primeiroValor * segundoValor + 
-               "</h2>");
+
+if (operacao == 1){ //multiplicação
+    document.write("<h2>" + primeiroValor + " * " + segundoValor + " = " + primeiroValor * segundoValor + "</h2>");    
+} else if (operacao ==2 ){ // soma
+    document.write("<h2>" + primeiroValor + " + " + segundoValor + " = " + primeiroValor + segundoValor + "</h2>");
+} else if (operacao ==3 ){ // subtração
+    document.write("<h2>" + primeiroValor + " - " + segundoValor + " = " + primeiroValor - segundoValor + "</h2>");
+}else if (operacao ==4 ){ // divisão
+        document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + primeiroValor / segundoValor + "</h2>");
+} else {
+    document.write("<h2> error </h2>")
+}

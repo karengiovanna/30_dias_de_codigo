1267da81c7ea94bbdc7a4f3fb99dac341cf1e1b0
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-26 20:03:00 -0300

    proteger os atributos e criar métodos
    Ao proteger atributos não conseguiremos mais acessar os valores. Precisamos criar métodos que nos possibilite receber os valores dos atributos, ou até alterá-los, caso seja interessante. Para isso criaremos os Getters e os Setters.

--- Java OO/bytebank-encapsulamento/src/Conta.java
+++ Java OO/bytebank-encapsulamento/src/Conta.java
@@ -1,21 +1,42 @@
 
 public class Conta {
-	double saldo;
-	int numero;
-	int agencia;
-	Cliente titular; /*eh o nome da classe */
+	private double saldo;
+	private int numero;
+	private int agencia;
+	private Cliente titular; /*eh o nome da classe */
 
 	
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
 	
-	public boolean saca(double valor) {
-		if (this.saldo > valor) {
-			this.saldo -= valor;
-			return true;
-		} else {
-			return false;
-		}
+	// Getter
+	public double getSaldo(){
+		return this.saldo;
+	}
+	public int getNumero(){
+		return this.numero;	
+	}
+	
+	public int getAgencia(){
+		return this.agencia;	
+	}
+	public getTitular(){
+		return this.titular;
+	}
+	
+	
+	
+	//Setter
+	public void setNumero(int numero){
+		this.numero = numero;	
+	}
+	
+	public void setAgencia(int agencia){
+		this.agencia = agencia;	
+	}
+	
+	public void setTitular(int titular){
+		this.titular = titular;
 	}
 }

585d7aed35a784c72aa02c8b747235fd23bf2807
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-26 00:39:31 -0300

    Create script.js

---
+++ Imersao Dev - ALURA/Mentalista/script.js
@@ -0,0 +1,9 @@
+var numeroSecreto = 3
+
+var chute = parseInt(prompt("Digite um numero entre 0 e 10"))
+
+if(numeroSecreto == chute){
+  alert("acertou")
+} else {
+  alert("errou, o número secreto era: " + numeroSecreto)
+}

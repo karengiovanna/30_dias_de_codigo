ee11baefcfca3f50c6264284854adfc6b01aa25f
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-26 02:27:55 -0300

    Array em JS

--- Imersao Dev - ALURA/3 - AluraFlix/script.js
+++ Imersao Dev - ALURA/3 - AluraFlix/script.js
@@ -1,7 +1,11 @@
-var filme1 = "Hannah Montana"
-var filme2 = "Zathura"
-var filme3 = "The handmaiden"
+// var filme1 = "Hannah Montana"
+// var filme2 = "Zathura"
+// var filme3 = "The handmaiden"
 
-console.log(filme1)
-console.log(filme2)
-console.log(filme3)
+var filmes = ["Hannah Montana", "Zathura", "The Handmaiden"]
+
+//console.log(filme1)
+//console.log(filme2)
+//console.log(filme3)
+
+console.log(filmes)

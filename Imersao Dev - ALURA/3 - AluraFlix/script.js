0ea738bbfbbfdf1de8c88fb7760e455be5be5e90
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-26 02:36:10 -0300

    chamar elementos do array

--- Imersao Dev - ALURA/3 - AluraFlix/script.js
+++ Imersao Dev - ALURA/3 - AluraFlix/script.js
@@ -4,8 +4,19 @@
 
 var filmes = ["Hannah Montana", "Zathura", "The Handmaiden"]
 
+// var filmes = []
+filmes.push("Hannah Montana") // push para inserir valor dentro do array
+
+
 //console.log(filme1)
 //console.log(filme2)
 //console.log(filme3)
 
-console.log(filmes)
+// chamar todos os elementos
+// console.log(filmes)
+
+// chamar apenas um elemento
+console.log(filmes[1])
+
+// tamanho do array
+console.log(filmes.length)

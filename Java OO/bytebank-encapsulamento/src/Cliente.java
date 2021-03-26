1239631c356428b63184eb516a101f0cd1c84f6c
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-26 20:17:23 -0300

    Criando getters e setters

--- Java OO/bytebank-encapsulamento/src/Cliente.java
+++ Java OO/bytebank-encapsulamento/src/Cliente.java
@@ -1,6 +1,33 @@
 /* toda conta tem um cliente (toda conta tem uma referencia para um cliente) */
 public class Cliente {
-		 String nome;
-		 String cpf;
-		 String profissao;
+	 private String nome;
+	 private String cpf;
+	 private String profissao;
+
+	// setters
+	public void setNome(nome){
+		this.nome = nome;
+	}
+	
+	public void setCPF(cpf){
+		this.cpf = cpf;	
+	}
+	
+	public void setProfissao(){
+		this.profissao = profissao;
+	}
+	
+	// getters
+	
+	public string getNome(){
+		return this.nome;
+	}
+	
+	public string getCPF(){
+		return this.cpf;
+	}
+	
+	public string getProfissao(){
+		return this.profissao;
+	}
 }

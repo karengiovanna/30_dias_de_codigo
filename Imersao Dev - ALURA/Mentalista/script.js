83dbdeab934d0367687c326f38aee990cf8c4d44
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-26 01:23:09 -0300

    Inserção do laço while

--- Imersao Dev - ALURA/Mentalista/script.js
+++ Imersao Dev - ALURA/Mentalista/script.js
@@ -1,9 +1,20 @@
 var numeroSecreto = 3
+var tentativa = 3
 
-var chute = parseInt(prompt("Digite um numero entre 0 e 10"))
+while (tentativa>0){
+  var chute = parseInt(prompt("Digite um numero entre 0 e 10"))
+  
+  if(numeroSecreto == chute){
+    console.log("acertou")
+    break
+  
+  } else if (numeroSecreto < chute) {
+    console.log("Errado, o numero secreto é menor que o seu chute")
+    tentativa = tentativa - 1
+  
+  } else if (numeroSecreto > chute){
+    console.log("Errou, o número secreto é maior do que o seu chute")
+    tentativa = tentativa - 1
+  }
 
-if(numeroSecreto == chute){
-  console.log("acertou")
-} else {
-  console.log("Errado, o numero secreto é " + numeroSecreto)
 }

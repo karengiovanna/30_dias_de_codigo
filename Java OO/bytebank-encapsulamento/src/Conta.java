df3cdc406b8ef6a1e2c215a6ea5af6920de8d0aa
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-26 21:56:59 -0300

    Adicionando um construtor para a classe Conta

--- Java OO/bytebank-encapsulamento/src/Conta.java
+++ Java OO/bytebank-encapsulamento/src/Conta.java
@@ -4,10 +4,23 @@ public class Conta {
 	private int numero;
 	private int agencia;
 	private Cliente titular; /*eh o nome da classe */
-
+	
+	// o java so da um construtor padrao que ao faz nada apenas se vc nao tiver feito um construtor
+	// a partir do momento que vc faz um construtor
+	
+	public Conta(int numero, int agencia, String titular){
+		this.numero = numero;
+		this.agencia = agencia;
+		this.titular = titular;
+		System.out.println("Estou criando uma conta de numero " + numero + " e agencia " + agencia ". Cujo titular é: "+ titular);
+	}
 	
 	public void deposita(double valor) {
 		this.saldo += valor;
+	}
+	
+	public void saca(){
+		this.saldo +=	
 	}
 	
 	// Getter
@@ -29,10 +42,18 @@ public class Conta {
 	
 	//Setter
 	public void setNumero(int numero){
+		if (numero  <=0) {
+			System.out.println("O número da conta deve ser maior do que zero");
+			return;
+		}
 		this.numero = numero;	
 	}
 	
 	public void setAgencia(int agencia){
+		if (agencia <=0){
+			System.out.println("não pode valor menor ou igual a zero");
+			return;
+		}
 		this.agencia = agencia;	
 	}
 	

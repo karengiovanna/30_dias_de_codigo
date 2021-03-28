d1a31cae64514e9cc56c480df2d3d334a6876a14
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-28 01:06:44 -0300

    exibir array de imagens

--- Imersao Dev - ALURA/3 - AluraFlix/script.js
+++ Imersao Dev - ALURA/3 - AluraFlix/script.js
@@ -2,10 +2,10 @@
 // var filme2 = "Zathura"
 // var filme3 = "The handmaiden"
 
-var filmes = ["Hannah Montana", "Zathura", "The Handmaiden"]
+//var listaFilmes = ["Hannah Montana", "Zathura", "The Handmaiden"]
 
 // var filmes = []
-filmes.push("Hannah Montana") // push para inserir valor dentro do array
+//listaFilmes.push("Hannah Montana") // push para inserir valor dentro do array
 
 
 //console.log(filme1)
@@ -16,7 +16,18 @@ filmes.push("Hannah Montana") // push para inserir valor dentro do array
 // console.log(filmes)
 
 // chamar apenas um elemento
-console.log(filmes[1])
+//console.log(filmes[1])
 
 // tamanho do array
-console.log(filmes.length)
+//console.log(filmes.length)
+
+listaFilmes = []
+listaFilmes.push("https://m.media-amazon.com/images/M/MV5BMDc1NzM1OTgtOGYxMS00ZWE1LWEzZWMtNWEyOTI2NDJjOTU2XkEyXkFqcGdeQXVyNjQzNzA2NjM@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+listaFilmes.push("https://m.media-amazon.com/images/M/MV5BMTkxNjUxODY3NF5BMl5BanBnXkFtZTcwMjQyMzMzMg@@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+listaFilmes.push("https://m.media-amazon.com/images/M/MV5BNDJhYTk2MTctZmVmOS00OTViLTgxNjQtMzQxOTRiMDdmNGRjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR2,0,182,268_AL_.jpg")
+
+for(var i =0; i< listaFilmes.length; i++){
+  document.write("<img src = " + listaFilmes[i] + ">")
+}

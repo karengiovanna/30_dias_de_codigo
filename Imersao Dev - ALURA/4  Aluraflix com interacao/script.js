c847d77e2e8fb41ff3848758b731b74104209b26
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-28 17:55:25 -0300

    Update script.js

--- Imersao Dev - ALURA/4  Aluraflix com interacao/script.js
+++ Imersao Dev - ALURA/4  Aluraflix com interacao/script.js
@@ -1,11 +1,16 @@
 function adicionarFilme(){
   var campoFilmeFavorito = document.querySelector('#filme')
   var filmeFavorito = campoFilmeFavorito.value
-  listarFilmesNaTela(filmeFavorito)
+  if (filmeFavorito.endsWith('.jpg')){
+    listarFilmesNaTela(filmeFavorito)
+  }else{
+    alert("Valor inválido: insira um endereço de imagem jpg")
+  }
+  campoFilmeFavorito.value=""
  }
 
 function listarFilmesNaTela(filme){
-  var listaFilmes = document.querySelector('#listaFilmes')
-  var elementoFilme = "<img src="+ filme+">"
+  var listaFilmes = document.querySelector('#listaFilmes') //querySelector pega elementos do html
+  var elementoFilme = "<img src="+ filme+">" // monta o elemento com o img
   listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
  }

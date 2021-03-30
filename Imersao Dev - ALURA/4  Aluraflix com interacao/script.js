085db4d9267f9cb03c8512ba9e642b2922f18300
Karen Giovanna Melo <49700354+karengiovanna@users.noreply.github.com>
2021-03-30 05:42:01 -0300

    Criei as funções de buscar clipe, exibir todos e adicionar

--- Imersao Dev - ALURA/4  Aluraflix com interacao/script.js
+++ Imersao Dev - ALURA/4  Aluraflix com interacao/script.js
@@ -1,16 +1,48 @@
-function adicionarFilme(){
-  var campoFilmeFavorito = document.querySelector('#filme')
-  var filmeFavorito = campoFilmeFavorito.value
-  if (filmeFavorito.endsWith('.jpg')){
-    listarFilmesNaTela(filmeFavorito)
-  }else{
-    alert("Valor inválido: insira um endereço de imagem jpg")
-  }
-  campoFilmeFavorito.value=""
- }
 
-function listarFilmesNaTela(filme){
-  var listaFilmes = document.querySelector('#listaFilmes') //querySelector pega elementos do html
-  var elementoFilme = "<img src="+ filme+">" // monta o elemento com o img
-  listaFilmes.innerHTML = listaFilmes.innerHTML + elementoFilme
+var listaTitulosClipes = ['Tusa', 'Location']
+var listaEmbededClipes = [
+  '<iframe width="360" height="201" src="https://www.youtube.com/embed/tbneQDc2H3I" allowfullscreen></iframe>',
+'<iframe width="360" height="201" src="https://www.youtube.com/embed/aw_cmzF_uZY" allowfullscreen></iframe>']
+
+
+function buscarClipe(){
+    var titulo = document.getElementById("campo").value
+    for (var i=0; i<2; i++){
+        if(titulo == listaTitulosClipes[i]){
+          document.querySelector('#campo').value = ''
+          listarClipeNaTela(listaEmbededClipes[i])
+          break
+        }
+    }
+    alert("Esse clipe não está na nossa base de dados! Adcione o embeded no campo")
+    document.getElementById("campo").value = ""
+}
+
+
+function listarClipeNaTela(clipe){
+  document.querySelector('#listaClipes').innerHtml = clipe
+  var listaClipes = document.querySelector('#listaClipes') //querySelector pega elementos do html
+  listaClipes.innerHTML =  clipe
+
+}
+
+function exibirNaTela(){
+  tam = listaTitulosClipes.length
+  var listaFilmes = document.querySelector('#listaClipes') //querySelector pega elementos do html
+  listaFilmes.innerHTML = listaEmbededClipes[0]
+  for(var i= 1; i<tam; i++){
+    listaFilmes.innerHTML = listaFilmes.innerHTML + listaEmbededClipes[i]
+  }
+}
+
+function adicionarClipe(){
+  embeded = document.querySelector('#campo').value 
+  if (embeded.endsWith('</iframe>')){
+    listaEmbededClipes.push(embeded)
+    titulo = prompt("Insira o título do clipe")
+    listaTitulosClipes.push(titulo)
+  }else{
+    alert("Valor inválido: insira um iframe")
+  }
+  campo.value=""
  }
